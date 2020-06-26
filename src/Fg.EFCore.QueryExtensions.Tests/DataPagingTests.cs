f4853ec7ff2f6bcc83574e79dbbbf8e90f248a6f
Frederik Gheysels <frederik.gheysels@codit.eu>
2020-06-26 13:48:17 +0200

    Add support for paged EF queries

---
+++ src/Fg.EFCore.QueryExtensions.Tests/DataPagingTests.cs
@@ -0,0 +1,93 @@
+﻿using Fg.EFCore.QueryExtensions.Tests.Domain;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Fg.EFCore.QueryExtensions.Tests
+{
+    public class DataPagingTests : IDisposable
+    {
+        private readonly ITestOutputHelper _unitTestOutput;
+
+        private readonly NavyDbContext _dbContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilterExpressionTests"/> class.
+        /// </summary>
+        public DataPagingTests(ITestOutputHelper unitTestOutput)
+        {
+            _unitTestOutput = unitTestOutput;
+
+            var connection = new SqliteConnection("data source=:memory:");
+
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<NavyDbContext>()
+                          .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddXunit(_unitTestOutput)))
+                          .UseSqlite(connection).Options;
+
+            _dbContext = new NavyDbContext(options);
+
+            _dbContext.Database.EnsureCreated();
+
+            _dbContext.Vessels.Add(new Vessel { Id = 1, Name = "Enterprise" });
+            _dbContext.Vessels.Add(new Vessel { Id = 2, Name = "Nautilus" });
+            _dbContext.Vessels.Add(new Vessel { Id = 3, Name = "Boaty McBoatface" });
+            _dbContext.Vessels.Add(new Vessel { Id = 5, Name = "Nautica" });
+
+            _dbContext.SaveChanges();
+        }
+
+
+        [Fact]
+        public async Task CanRetrievePagedResults()
+        {
+            var result = await _dbContext.Vessels.OrderBy(v => v.Id).ToPagedResult(1, 2);
+
+            Assert.Equal(1, result.PageNumber);
+            Assert.Equal(2, result.Items.Count());
+            Assert.Equal(4, result.TotalNumberOfItems);
+
+            Assert.Contains(result.Items, v => v.Id == 1);
+            Assert.Contains(result.Items, v => v.Id == 2);
+        }
+
+        [Fact]
+        public async Task PagedResultsWithLastIncompletePage()
+        {
+            var firstPage = await _dbContext.Vessels.ToPagedResult(1, 3);
+
+            Assert.Equal(1, firstPage.PageNumber);
+            Assert.Equal(3, firstPage.Items.Count());
+            Assert.Equal(4, firstPage.TotalNumberOfItems);
+
+            var secondPage = await _dbContext.Vessels.ToPagedResult(2, 3);
+
+            Assert.Equal(2, secondPage.PageNumber);
+            Assert.Equal(1, secondPage.Items.Count());
+            Assert.Equal(4, secondPage.TotalNumberOfItems);
+        }
+
+        [Fact]
+        public async Task CanRetrievePagedResultsOnFilteredQuery()
+        {
+            var result = await _dbContext.Vessels
+                                         .Where(DbFilterExpression.LikeOneOf<Vessel>(nameof(Vessel.Name), new[] { "Nau%", "%stat%" }))
+                                         .ToPagedResult(1, 2);
+
+            Assert.Equal(1, result.PageNumber);
+            Assert.Equal(2, result.TotalNumberOfItems);
+        }
+
+        /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
+        public void Dispose()
+        {
+            _dbContext?.Dispose();
+        }
+    }
+}
---
+++ src/Fg.EFCore.QueryExtensions/DataPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fg.EFCore.QueryExtensions
+{
+    public class DataPage<T>
+    {
+        public int TotalNumberOfItems { get; }
+        public int PageNumber { get; }
+        public int PageSize { get; }
+        public int TotalPages { get; }
+        public IEnumerable<T> Items { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataPage"/> class.
+        /// </summary>
+        public DataPage(IEnumerable<T> items, int totalNumberOfItems, int pageNumber, int pageSize)
+        {
+            Items = items;
+            TotalNumberOfItems = totalNumberOfItems;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(TotalNumberOfItems / (double)pageSize);
+        }
+    }
+}
