d2ea39c131b038ca76ec68c344b4493f0a9e4f3c
Frederik Gheysels <frederik.gheysels@telenet.be>
2020-06-26 13:51:19 +0200

    Add support for paged EF queries (#4)

---
+++ src/Fg.EFCore.QueryExtensions/IQueryableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fg.EFCore.QueryExtensions
+{
+    public static class IQueryableExtensions
+    {
+        public static async Task<DataPage<T>> ToPagedResult<T>(this IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            int numberOfItems = await source.CountAsync();
+
+            int numberOfItemsToSkip = (pageNumber - 1) * pageSize;
+
+            var items = await source.Skip(numberOfItemsToSkip).Take(pageSize).ToListAsync();
+
+            return new DataPage<T>(items, numberOfItems, pageNumber, pageSize);
+        }
+    }
+}
